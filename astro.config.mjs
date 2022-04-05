afa947667177853ae16f9fe7a15da224d936b1b1
LindaKat <lindakt16@gmail.com>
2022-04-04 21:55:03 -0500

    initial commit

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,16 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 
+import vue from "@astrojs/vue";
+
 // https://astro.build/config
 export default defineConfig({
-	integrations: [preact()],
+  integrations: [preact(), vue()],
+  markdown: {
+    syntaxHighlight: 'shiki',
+    shikiConfig: {
+      theme: 'dracula',
+      wrap: true,
+    },
+  },
 });
