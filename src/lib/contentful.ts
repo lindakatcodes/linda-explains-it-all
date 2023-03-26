ac6098c1f9a79819e64733bc8febbaa366e123d6
LindaKat <lindakt16@gmail.com>
2023-03-26 01:41:50 -0500

    got post page working, sticking with rich text

--- src/lib/contentful.ts
+++ src/lib/contentful.ts
@@ -1,16 +1,5 @@
 import * as contentful from "contentful";
 
-import type { Document } from "@contentful/rich-text-types";
-
-export interface Post {
-  title: string;
-  slug: string;
-  body: Document;
-  tags: string;
-  date: string;
-  comments: boolean;
-}
-
 export const contentfulClient = contentful.createClient({
   space: import.meta.env.CONTENTFUL_SPACE_ID,
   accessToken: import.meta.env.DEV
@@ -18,3 +7,21 @@ export const contentfulClient = contentful.createClient({
     : import.meta.env.CONTENTFUL_DELIVERY_TOKEN,
   host: import.meta.env.DEV ? "preview.contentful.com" : "cdn.contentful.com",
 });
+
+// Contentful Model Types
+
+export interface TypePostFields {
+  title: contentful.EntryFields.Symbol;
+  slug?: contentful.EntryFields.Symbol;
+  topics?: contentful.Entry<TypeTopicFields>[];
+  body: contentful.EntryFields.RichText;
+  bodyMarkdown: contentful.EntryFields.Text;
+}
+
+export type TypePost = contentful.Entry<TypePostFields>;
+
+export interface TypeTopicFields {
+  name?: contentful.EntryFields.Symbol;
+}
+
+export type TypeTopic = contentful.Entry<TypeTopicFields>;
